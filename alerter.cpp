d8bead2996e5fff67df53f4984a6ebe5c6d9b050
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-10-20 20:27:41 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -3,6 +3,10 @@
 
 int alertFailureCount = 0;
 bool mockNetworkFailure = false;
+int failureOccured = 0;
+
+typedef int (*NetworkAlertFunc)(float);// to use the same code in Production without much changes. 
+
 
 int networkAlertStub(float celcius) {
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
@@ -10,14 +14,15 @@ int networkAlertStub(float celcius) {
     // Return 500 for not-ok
     // stub always succeeds and returns 200
     if(mockNetworkFailure){ 
+        failureOccured += 1;
         return 500;
     }
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
+void alertInCelcius(float farenheit, NetworkAlertFunc networkAlert) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = networkAlert(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -29,16 +34,17 @@ void alertInCelcius(float farenheit) {
 
 void testAlertFailureCount(){
     mockNetworkFailure = false;
-    alertInCelcius(400.5);
-    assert(alertFailureCount == 0);
+    alertInCelcius(400.5,networkAlertStub);
+    assert(alertFailureCount == failureOccured);
+      
 
      mockNetworkFailure = true;
-    alertInCelcius(303.6);
-     assert(alertFailureCount == 1);
+    alertInCelcius(303.6,networkAlertStub);
+    assert(alertFailureCount == failureOccured);    
 
     mockNetworkFailure = true;
-    alertInCelcius(320.6);
-     assert(alertFailureCount == 2);
+    alertInCelcius(320.6,networkAlertStub);
+    assert(alertFailureCount == failureOccured);
 
 }
 
